11152ede30744339e60bd2315086d7b9d8948ed0
blishchenko1981 <blishchenko1981@gmail.com>
2021-07-19 16:45:37 -0400

    steps

--- src/GitCool.java
+++ src/GitCool.java
@@ -3,4 +3,8 @@ public class GitCool {
     // I so good in Java thaat gona get job in first week))))))
 
     // we had added more to check how is everything work
+    // Share Project on github happen only once
+
+
+    //create rep, link. push
 }
